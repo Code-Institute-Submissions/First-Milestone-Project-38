bb1ee836f0164723a710c7c5496fc2bb4f0eca78
GaryFinlayson <gfinlayson1@hotmail.co.uk>
2019-11-20 10:15:37 +0000

    Added Photography page and styled the Footer.

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,11 +1,22 @@
 
 $(document).ready(function(){
 
-  $(".footer-links").hide();
+  checkScreenWidth();
+  
+});
 
+function checkScreenWidth() {
+  if (window.matchMedia('(max-width: 991px)').matches) {
+
+    $(".footer-links").hide();
+    
     $(".my-social").click(function(){
       $(".footer-links").slideToggle("slow");
       $(".arrow.up").toggleClass("down");
     });
 
-  });
+  } else {
+    $(".footer-links").show();
+    $(".arrow")
+  }
+}
