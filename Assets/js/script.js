669186b5284ff46d38042ff3fed08fac54b7e523
GaryFinlayson <gfinlayson1@hotmail.co.uk>
2020-01-21 13:49:35 +0000

    Small changes to .js to fix bugs with the screen to responding correctly to resizing

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -4,18 +4,21 @@ $(document).ready(function(){
   
 });
 
+var mql = window.matchMedia('(max-width: 991px');
+
 function checkScreenWidth() {
   if (window.matchMedia('(max-width: 991px)').matches) {
 
-    $(".footer-links").hide();
+    //$(".footer-links").hide();
     
     $(".my-social").click(function(){
       $(".footer-links").slideToggle("slow");
       $(".arrow.up").toggleClass("down");
     });
 
-  } else {
-    $(".footer-links").show();
-    $(".arrow")
-  }
+  } //else {
+    //$(".footer-links").show();
+  //}
 }
+
+mql.addListener(checkScreenWidth);
